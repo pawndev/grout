f08cd2f69123de2fa9ef800130ede514adb5f392
Brandon T. Kowalski <brandon@kowalski.io>
2026-01-11 18:34:35 -0500

    Tidy up taskfile, removing old i18n scripts

--- ui/status_bar.go
+++ ui/status_bar.go
@@ -2,13 +2,18 @@ package ui
 
 import (
 	gaba "github.com/BrandonKowalski/gabagool/v2/pkg/gabagool"
+	icons "github.com/BrandonKowalski/gabagool/v2/pkg/gabagool/constants"
 )
 
 var defaultStatusBar = gaba.StatusBarOptions{
 	Enabled:    true,
 	ShowTime:   true,
 	TimeFormat: gaba.TimeFormat24Hour,
-	Icons:      []gaba.StatusBarIcon{},
+	Icons: []gaba.StatusBarIcon{
+		{
+			Text: icons.WiFi,
+		},
+	},
 }
 
 func StatusBar() gaba.StatusBarOptions {
